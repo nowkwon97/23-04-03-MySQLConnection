83a443484d4fd7ae6953531d7083c1f68dd3a4ac
leejaekwon <nowkwon97@gmail.com>
2023-04-03 17:43:11 +0900

    서버 연결 테스트

--- index.js
+++ index.js
@@ -2,26 +2,38 @@ const mysql = require('mysql');
 const http = require('http');
 const fs = require('fs').promises;
 
-const connection = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password : '0000',
-  database : 'test'
-});
+// // mysql 정보 부분
+// const connection = mysql.createConnection({
+//   host : 'localhost',
+//   user : 'root',
+//   password : '0000',
+//   database : 'test'
+// });
+// // mysql 연결
+// connection.connect();
+// // query를 이용하여 mysql 작업
+// connection.query('SELECT * FROM user', function(error, results, fields) {
+//   if(error) {
+//     console.log(error);
+//   }
+//   console.log(results);
+// })
+// // mysql 연결 종료
+// connection.end();
 
-connection.connect();
 
-connection.query('SELECT * FROM user', function(error, results, fields) {
-  if(error) {
-    console.log(error);
+// 서버 연결 부분
+http.createServer(async (req, res) => {
+  try {
+    const data = await fs.readFile('./index.html');
+    res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+    res.end(data);
+  } catch (err) {
+    console.error(err);
+    res.writeHead(500, {'Content-Type' : 'text/html; charset=utf-8'});
+    res.end(err.message);
   }
-  console.log(results);
 })
-
-connection.end();
-
-const server = http.createServer(function(req, res) {
-  const mainPage = fs.readFile('./index.html');
-  res.writeHead(200, {'Content-type': 'text/html'});
-  res.end(mainPage);
-})
+.listen(8080, () => {
+  console.log('8080번 포트에 서버 대기중');
+});
