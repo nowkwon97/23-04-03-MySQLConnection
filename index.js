2418fb5ca762d15673126591bb0a3884ec005fb9
leejaekwon <nowkwon97@gmail.com>
2023-04-03 17:36:39 +0900

    간단한 로그인 페이지 작성

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql');
+const http = require('http');
+const fs = require('fs').promises;
 
 const connection = mysql.createConnection({
   host : 'localhost',
@@ -6,3 +8,20 @@ const connection = mysql.createConnection({
   password : '0000',
   database : 'test'
 });
+
+connection.connect();
+
+connection.query('SELECT * FROM user', function(error, results, fields) {
+  if(error) {
+    console.log(error);
+  }
+  console.log(results);
+})
+
+connection.end();
+
+const server = http.createServer(function(req, res) {
+  const mainPage = fs.readFile('./index.html');
+  res.writeHead(200, {'Content-type': 'text/html'});
+  res.end(mainPage);
+})
