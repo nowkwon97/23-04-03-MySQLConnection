7a859df88f10dc966c25b58bfd640533c11e7a2c
leejaekwon <nowkwon97@gmail.com>
2023-04-03 20:22:58 +0900

    mysql 오류 해결 -> mysql2사용

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const http = require('http');
 const fs = require('fs').promises;
 
@@ -6,14 +6,21 @@ const fs = require('fs').promises;
 const connection = mysql.createConnection({
   host : 'localhost',
   user : 'root',
-  password : '0000',
-  database : 'test'
+  password : 'cbs8807232^^',
+  database : 'user'
 });
 // mysql 연결
 connection.connect();
 
 // 서버 연결 부분
 http.createServer(async (req, res) => {
+  // query를 이용하여 mysql 작업
+  connection.query('SELECT * FROM users', function(error, results, fields) {
+    if(error) {
+      console.log(error);
+    }
+    console.log(results);
+})
   try {
     const data = await fs.readFile('./index.html');
     res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
@@ -24,18 +31,13 @@ http.createServer(async (req, res) => {
     res.end(err.message);
   }
 })
+
 .listen(8080, () => {
   console.log('8080번 포트에 서버 대기중');
 });
 
-// query를 이용하여 mysql 작업
-connection.query('SELECT * FROM user', function(error, results, fields) {
-  if(error) {
-    console.log(error);
-  }
-  console.log(results);
-})
-// mysql 연결 종료
-connection.end();
+
+// // mysql 연결 종료
+// connection.end();
 
 
